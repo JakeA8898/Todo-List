280746fa29e62983a37c576914937d2c6fae510c
Jacob <36414267+JakeA8898@users.noreply.github.com>
2019-04-01 18:03:18 -0400

    started adding footer buttons to filter tasks

--- todo/script.js
+++ todo/script.js
@@ -2,6 +2,8 @@ var button = document.getElementById("enter");
 var clear = document.getElementById("clear");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var listItems = [];
+var index = 0;
 
 function inputLength() {
 	return input.value.length;
@@ -12,7 +14,17 @@ function createListElement() {
 	var li = document.createElement("li");
 	var complete = document.createElement("input");
 	var del = document.createElement("button");
+	
 	div.classList.add("list-wrapper");
+	div.classList.add("visible");
+	div.classList.toggle("visible");
+
+	var item = {
+		thisDiv: div,
+		itemIndex: index
+	};
+	index++;
+
 	complete.type = "checkbox";
 	complete.addEventListener("click",function(){
 		li.classList.toggle("done");
@@ -21,16 +33,25 @@ function createListElement() {
 	del.appendChild(document.createTextNode("Delete"));
 	del.addEventListener("click",function(){
 		ul.removeChild(div);
+		listItems.splice(item.itemIndex,1);
+		for(var i = item.itemIndex;i < listItems.length;i++){
+			listItems[i].itemIndex -=1;
+		}
+
+		
 	});
 
 	li.classList.add("done")
 	li.classList.add("listElem");
 	li.classList.toggle("done");
 
+	
+
 	li.appendChild(document.createTextNode(input.value));
+	div.appendChild(li);
 	div.appendChild(complete);
 	div.appendChild(del);
-	div.appendChild(li);
+	listItems.push(item);
 	ul.appendChild(div);
 	input.value = "";
 }
@@ -45,11 +66,14 @@ function addListAfterClick() {
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
+
 	}
 }
 
 function clearListAfterClick(){
 	ul.innerHTML = "";
+	input.value = "";
+	listItems = [];
 }
 
 button.addEventListener("click", addListAfterClick);
